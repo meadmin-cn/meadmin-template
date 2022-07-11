88edeb499f9cecf34604e8bfbbd9da06f961d923
yuntian001 <479820787@qq.com>
2022-07-11 10:59:19 +0800

    ci: 加上提交信息校验遵循conventional commits

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+let releaseItConfig =require('./.release-it.json');
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-empty': [2, 'never'], // type 不为空
+      'type-enum': [2, 'always', releaseItConfig.plugins['@release-it/conventional-changelog'].preset.types.map(item=>item.type)],
+    }
+};
