ac09e3c7b59c5b289147a21bfdbbaee814eab0ea
yuntian001 <479820787@qq.com>
2022-07-23 11:58:02 +0800

    ignore: 删除多余文件

--- src/store/module.ts
+++
@@ -1,5 +0,0 @@
-//当前文件由vite-plugin-autogeneration-import-file自动生成
-export { default as useGlobalStore } from "./modules/global"
-export { default as useSettingStore } from "./modules/setting"
-export { default as useUserStore } from "./modules/user"
-//code
