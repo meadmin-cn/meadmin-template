52d1b36068b9167c0b828d07c67d16af67f242e7
yuntian001 <479820787@qq.com>
2022-08-22 17:08:17 +0800

    fix: vue route 不支持json格式化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="index">
     hello word!
-    <input name="text" v-model="value" />
+    <input name="text" />
   </div>
 </template>
-<script setup lang="ts" name="Dashboard">
-  const value=ref('')
-</script>
+<script setup lang="ts" name="Dashboard"></script>
 
 <style lang="scss" scoped>
 .index {
