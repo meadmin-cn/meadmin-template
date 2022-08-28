f00aff0dfa102b082d2842e1e73f16d1ddbc0732
yuntian001 <479820787@qq.com>
2022-08-28 22:12:22 +0800

    ignore: 控制台首页，只完成头部统计数据视图

---
+++ src/views/dashboard/components/totalPanel.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="total-panel">
+    <el-row :gutter="20">
+      <el-col v-for="(item, index) in list" :key="index" :xs="24" :sm="12" :xl="6">
+        <el-card shadow="hover" :header="index == 0 ? '' : item.title" class="total-item">
+          <el-skeleton :loading="index == 0">
+            <div class="content">
+              <div class="total">
+                <me-number :end="item.total" :format="undefined"></me-number>
+                <component :is="item.icon" :style="{ color: item.iconColor }" />
+              </div>
+              <div class="footer">
+                {{ item.subTitle }}<span>{{ item.subTotal }}</span>
+              </div>
+            </div>
+          </el-skeleton>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+const list = reactive([
+  {
+    title: '用户数',
+    icon: 'mel-icon-user-filled',
+    iconColor: '#67C23A',
+    total: 3000,
+    subTitle: '昨日新增：',
+    subTotal: 300,
+  },
+  {
+    title: '订单数',
+    icon: 'mel-icon-histogram',
+    iconColor: '#E6A23C',
+    total: 299,
+    subTitle: '昨日新增：',
+    subTotal: 35,
+  },
+  {
+    title: '成交额',
+    icon: 'mel-icon-money',
+    iconColor: '#F56C6C',
+    total: '￥3815566.88',
+    subTitle: '昨日新增：',
+    subTotal: 25863,
+  },
+  {
+    title: '访问量',
+    icon: 'mel-icon-platform',
+    iconColor: '#409EFF',
+    total: 26589,
+    subTitle: '昨日新增：',
+    subTotal: 6930,
+  },
+]);
+</script>
+<style lang="scss" scoped>
+.total-panel {
+  .total-item {
+    --el-card-padding: 10px;
+    margin-bottom: 10px;
+    .content {
+      height: 91.5px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 0 10px;
+      .total {
+        font-size: 24px;
+        font-weight: bold;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-grow: 1;
+        .el-icon,
+        :deep(svg) {
+          height: 100%;
+          width: auto;
+        }
+      }
+      .footer {
+        font-size: 13px;
+        color: var(--el-text-color-regular);
+        span {
+          font-weight: bold;
+          color: var(--el-text-color-primary);
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/dashboard/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dashboard">
+    <total-panel></total-panel>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TotalPanel from './components/totalPanel.vue';
+</script>
+<style lang="scss" scoped>
+.dashboard {
+}
+</style>
