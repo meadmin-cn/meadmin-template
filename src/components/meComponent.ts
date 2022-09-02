f14edadaa5c9cf6b7ef5b898e70f3a135ef54b34
yuntian001 <479820787@qq.com>
2022-09-02 17:25:06 +0800

    fix: 修复多级菜单无法正常加载bug

--- src/components/meComponent.ts
+++ src/components/meComponent.ts
@@ -4,6 +4,7 @@ import { useLoadMessages } from '@/locales/i18n';
 import { done } from '@/utils/nProgress';
 import { localeConfig } from '@/config';
 export default defineComponent({
+  name: 'meComponent',
   props: {
     is: {
       type: [String, Object],
@@ -44,6 +45,7 @@ export default defineComponent({
       );
       if (props.keepAlive) {
         const index = components.length - 1;
+        console.log(index);
         components.push(h(MeKeepAlive, props.keepAlive, [components[index]]));
       }
       if (props.transition) {
