ba0add48f67ec7a49f2899b47e2505147dbdad3d
yuntian001 <479820787@qq.com>
2022-09-09 16:10:06 +0800

    ignore: 加上表格字段设置列的显隐

---
+++ src/components/meTable/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="me-table">
+    <div class="me-toolbar">
+      <div class="me-toolbar-search">
+        <slot name="search"></slot>
+      </div>
+      <div class="me-toolbar-content">
+        <slot name="toolbar"></slot>
+      </div>
+      <div class="me-toolbar-menu">
+        <div class="me-toolbar-buttons">
+          <slot name="buttons"></slot>
+        </div>
+        <div class="me-toolbar-tools">
+          <el-input placeholder="快捷搜索" prefix-icon="mel-icon-search" />
+          <el-button-group>
+            <el-popover placement="bottom" trigger="click" width="auto">
+              <template #reference>
+                <el-button icon="mel-icon-grid" title="自定义列" />
+              </template>
+              <template #default>
+                <el-tree
+                  node-key="value"
+                  @check-change="checkChange"
+                  :data="labels"
+                  default-expand-all
+                  :default-checked-keys="[...toRaw(checkedLabels)]"
+                  :props="{ label: 'label', children: 'children' }"
+                  show-checkbox
+                />
+              </template>
+            </el-popover>
+
+            <el-button icon="mel-icon-download" title="导出" />
+            <el-button icon="mel-icon-printer" title="打印" />
+            <slot name="tools"></slot>
+          </el-button-group>
+          <el-button>
+            <mel-icon-search></mel-icon-search>
+          </el-button>
+        </div>
+      </div>
+    </div>
+    <el-table v-bind="$attrs">
+      <component :is="children"></component>
+    </el-table>
+  </div>
+</template>
+<script lang="ts">
+import { ElTable } from 'element-plus';
+import customColumn from './hooks/customColumn';
+export default defineComponent<ComponentProps<typeof ElTable>>({
+  name: 'MeTable',
+  props: {} as any,
+  setup(props, { slots }) {
+    const { children, labels, checkedLabels } = customColumn(slots.default!);
+    const checkChange = (data: { value: string }, is: boolean, childrenIs: boolean) => {
+      if (is || childrenIs) {
+        checkedLabels.add(data.value);
+      } else {
+        checkedLabels.delete(data.value);
+      }
+    };
+    return { children, labels, checkedLabels, checkChange, toRaw };
+  },
+  mounted() {
+    console.log(this);
+  },
+});
+</script>
+<style lang="scss" scoped>
+.me-table {
+  margin-top: -10px;
+  .me-toolbar {
+    margin-top: 10px;
+    .me-toolbar-search {
+    }
+    .me-toolbar-content {
+    }
+    .me-toolbar-menu {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .me-toolbar-buttons {
+      }
+      .me-toolbar-tools {
+        display: flex;
+        align-items: center;
+        > div,
+        > span,
+        > button {
+          margin-left: 10px;
+        }
+        > div:first-child,
+        > span:first-child,
+        > button:first-child {
+          margin-left: 0;
+        }
+        > .el-button-group {
+          flex-shrink: 0;
+        }
+      }
+    }
+  }
+
+  :deep(.el-table) {
+    margin-top: 10px;
+  }
+}
+</style>
--- src/views/components/table.vue
+++ src/views/components/table.vue
@@ -1,11 +1,78 @@
 <template>
   <div class="table">
-    <el-table mb-1 :data="[]" />
-    <el-pagination :total="100" />
+    <me-table :data="tableData">
+      <el-table-column prop="date" label="Date" width="150" />
+      <el-table-column label="Delivery Info">
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column label="Address Info">
+          <el-table-column prop="state" label="State" width="120" />
+          <el-table-column prop="city" label="City" width="120" />
+          <el-table-column prop="address" label="Address" />
+          <el-table-column prop="zip" label="Zip" width="120" />
+        </el-table-column>
+      </el-table-column>
+    </me-table>
   </div>
 </template>
 <script setup lang="ts" name="Table">
-const a = 1;
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+];
 </script>
 <style lang="scss" scoped>
 .table {
