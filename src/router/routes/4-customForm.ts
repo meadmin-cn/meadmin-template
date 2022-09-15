bb0d704e0cd5e4386f538e5e3a5bdca04c773d01
yuntian001 <479820787@qq.com>
2022-09-15 21:28:57 +0800

    feat: 加上自定义表单

---
+++ src/router/routes/4-customForm.ts
@@ -0,0 +1,16 @@
+import { RouteRecordRaw } from 'vue-router';
+import Layout from '@/layout/index.vue';
+export const routes: RouteRecordRaw[] = [
+  {
+    path: '/customForm',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/customForm.vue'),
+        meta: { title: '自定义表单', icon: 'mel-icon-set-up' },
+      },
+    ],
+    meta: { title: '自定义表单', icon: 'mel-icon-set-up' },
+  },
+];
---
+++ src/views/customForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="custom-form">
+    <el-design-form></el-design-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ElementPlus from 'element-plus';
+import { ElDesignForm, ElGenerateForm } from 'vue-form-create';
+console.log(getCurrentInstance()?.appContext.app.use(ElementPlus));
+</script>
+<style lang="scss" scoped>
+.custom-form {
+  width: 100%;
+  height: 100%;
+}
+</style>
