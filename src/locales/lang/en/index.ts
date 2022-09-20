b92081a5c9f7a2b3970fd80fd0c4a8bf9fa9b2d6
jiayun zhang <a479820787@outlook.com>
2022-09-20 09:09:24 +0800

    feat: 全局语言包支持引入ts、json格式，支持子孙文件夹引入

--- src/locales/lang/en/index.ts
+++ src/locales/lang/en/index.ts
@@ -1,5 +1,5 @@
 import { forOwn } from 'lodash-es';
-const modules = import.meta.glob('./*.json', { eager: true, import: 'default' });
+const modules = import.meta.glob('./**/*.{json,ts}', { eager: true, import: 'default' });
 const langs = {} as Record<string, string>;
 forOwn(modules, (value) => {
   Object.assign(langs, value);
