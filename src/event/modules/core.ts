fe8c558f76378d1c9266016989713d34aabfcaf1
yuntian001 <479820787@qq.com>
2022-09-22 14:48:39 +0800

    fix: 修复跟字体不生效bug

--- src/event/modules/core.ts
+++ src/event/modules/core.ts
@@ -1,8 +1,6 @@
 import { installRoute } from '@/router';
 import { installStore } from '@/store';
 import { event, mitter } from '../index';
-import 'element-plus/dist/index.css';
-import 'element-plus/theme-chalk/dark/css-vars.css';
 import { installIcon } from '@/icons';
 import { installI18n } from '@/locales/i18n';
 import nProgress from 'nprogress';
