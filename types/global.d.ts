a1b08a6e3bda3da9e732c90ed318bf4e5b7998c7
yuntian001 <479820787@qq.com>
2022-09-22 20:20:06 +0800

    chore: 加上element函数声明

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,10 @@
 export {};
 declare global {
+  //unplugin-auto-import的Element是在页面加载到的时候才生成类型，这里全局声明一下
+  const ElLoading: typeof import('element-plus/es')['ElLoading'];
+  const ElMessage: typeof import('element-plus/es')['ElMessage'];
+  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox'];
+  const ElNotification: typeof import('element-plus/es')['ElNotification'];
   // svg-icon /el-cion-*组件定义
   type Icon = DefineComponent<{
     size: (StringConstructor | NumberConstructor)[];
