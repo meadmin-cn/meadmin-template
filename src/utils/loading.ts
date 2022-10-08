6781d6179fb5e0e4f5bf86c6f91ea588078e3c71
yuntian001 <yuntian001z@qq.com>
2022-10-08 11:12:38 +0800

    fix: 将请求loading改为使用全局loading

--- src/utils/loading.ts
+++ src/utils/loading.ts
@@ -1,7 +1,8 @@
 import { useGlobalStore } from '@/store';
 import { LoadingOptions } from 'element-plus';
-import { throttle, transform } from 'lodash-es';
+import { throttle } from 'lodash-es';
 let loadingInstance: ReturnType<typeof ElLoading.service>;
+export type LoadingMode = 'auto'|'global'|'page'
 class Loading {
   constructor(private execLoading: (options?: LoadingOptions) => void, private execClose: () => void) {
     this.execLoading = execLoading;
@@ -34,29 +35,30 @@ class Loading {
     this.throttleClose.cancel();
   }
 }
-const globalLoading = new Loading(
+export const globalLoading = new Loading(
   (options?: LoadingOptions) => (loadingInstance = ElLoading.service(options)),
   () => loadingInstance.close(),
 );
-const layoutLoading = new Loading(
+export const layoutLoading = new Loading(
   (options?: LoadingOptions) => (useGlobalStore().loadingOptions = options ? reactive(options) : {}),
   () => (useGlobalStore().loadingOptions = undefined),
 );
-function getLoading(forceGlobal = false) {
-  if (!forceGlobal && useGlobalStore().layoutLoaded) {
+function getLoading(mode:LoadingMode) {
+  if (mode === 'page' || ( mode ==='auto' && useGlobalStore().layoutLoaded)) {
     return layoutLoading;
   }
   return globalLoading;
 }
 
-export function loading(options?: LoadingOptions, number = 1, forceGlobal = false): void {
-  getLoading(forceGlobal).loading(options, number);
+export function loading(options?: LoadingOptions, number = 1, mode:LoadingMode = 'auto'): void {
+  getLoading(mode).loading(options, number);
 }
 
-export function closeLoading(force = false, number = 1, forceGlobal = false): void {
+
+export function closeLoading(force = false, number = 1, mode:LoadingMode = 'auto'): void {
   if (force) {
-    getLoading(forceGlobal).forceClose();
+    getLoading(mode).forceClose();
     return;
   }
-  getLoading(forceGlobal).close(number);
+  getLoading(mode).close(number);
 }
