933e84246b41743d895e3aa87714fe2bc59c0ead
yuntian001 <yuntian001z@qq.com>
2022-10-28 14:13:12 +0800

    refactor: 判断改为用全局变量$start

--- src/components/meVxeTable/install.ts
+++ src/components/meVxeTable/install.ts
@@ -4,7 +4,7 @@ import VXETablePluginElement from 'vxe-table-plugin-element';
 import 'vxe-table-plugin-element/dist/style.css';
 import './style.scss';
 VXETable.use(VXETablePluginElement);
-if (app.config.globalProperties.$t) {
+if (app.config.globalProperties.$start) {
   //为了兼容热更新重新拉取
   VXETable.setup({
     // 对组件内置的提示语进行国际化翻译
