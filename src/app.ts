ec9cbc9fa74811bfeb0e0450f56874f65133a57d
yuntian001 <yuntian001z@qq.com>
2022-10-28 14:02:44 +0800

    feat: 加上app create start 表示以便判断是否是热更新引用拿到

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,11 @@ import { event, mitter } from './event';
 import layoutMenuItem from '@/layout/components/menu/components/menuItem.vue';
 export const app = createApp(App);
 export async function bootscrapt() {
+<<<<<<< HEAD
   app.config.globalProperties.$start = true;
+=======
+  app.config.globalProperties.start = true;
+>>>>>>> cb1a17b (feat: 加上app create start 表示以便判断是否是热更新引用拿到)
   app.component('LayoutMenuItem', layoutMenuItem);
   await Promise.allSettled(mitter.emit(event.START, app));
   mitter.emit(event.READY, app);
