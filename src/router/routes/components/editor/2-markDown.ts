6da110c81eef7c7f55408a4c501c6c6001fc7450
yuntian001 <yuntian001z@qq.com>
2022-11-04 16:35:36 +0800

    feat: 加上markdown编辑器demo

---
+++ src/router/routes/components/editor/2-markDown.ts
@@ -0,0 +1,8 @@
+import { RouteRecordRaw } from 'vue-router';
+export const routes: RouteRecordRaw[] = [
+  {
+    path: 'markdown',
+    component: async () => await import('@/views/components/editor/markdwonEditor.vue'),
+    meta: { title: 'markdown' },
+  },
+];
