5c01ed522b0080deac654c5719f8175fc169e8ce
yuntian001 <yuntian001z@qq.com>
2022-11-07 23:31:56 +0800

    feat: 请求示例加上自动请求示例

--- src/api/example.ts
+++ src/api/example.ts
@@ -1,5 +1,4 @@
-import request from '@/utils/request';
-
+import { request, RequestOptions } from '@/utils/request';
 const enum Api {
   LIST = '/api/list',
   INFO = '/api/list/info/',
@@ -14,14 +13,16 @@ export interface ListResult {
   total: number;
   list: string[];
 }
-export function listApi() {
-  return request<ListResult, [ListParams]>((params) => ({
-    url: Api.LIST,
-    method: 'get',
-    params,
-  }));
+export function listApi(options: RequestOptions<ListResult, [ListParams]>) {
+  return request(
+    (params) => ({
+      url: Api.LIST,
+      method: 'get',
+      params,
+    }),
+    options,
+  );
 }
-
 export function infoApi() {
   return request<string, [number]>((id) => ({
     url: Api.INFO + id,
