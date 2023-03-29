1f07048713d979ec8920c0bd637876ba449b5286
yuntian001 <yuntian001z@qq.com>
2023-03-29 17:30:59 +0800

    feat: 优化全局类型

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,4 @@
-export {};
+import { DefineComponent } from 'vue';
 declare global {
   //unplugin-auto-import的Element是在页面加载到的时候才生成类型，这里全局声明一下
   const ElLoading: typeof import('element-plus/es')['ElLoading'];
@@ -10,13 +10,11 @@ declare global {
     size: (StringConstructor | NumberConstructor)[];
     color: StringConstructor;
   }>;
-  /* eslint-disable */
-  declare const __SSR__: boolean;
-  declare const __DEV__: boolean;
-  declare const __COMPAT__: boolean;
 
-  // Feature flags
-  declare const __FEATURE_PROD_DEVTOOLS__: boolean;
-  declare const __FEATURE_SUSPENSE__: boolean;
-  /* eslint-disable */
+  type ComponentProps<Component> = {
+    -readonly [K in keyof Omit<
+      InstanceType<Component>['$props'],
+      keyof InstanceType<DefineComponent>['$props']
+    >]: InstanceType<Component>['$props'][K];
+  };
 }
