223ff0d4ba240366b5339a09fce36d00f8e04676
yuntian001 <yuntian001z@qq.com>
2023-06-21 10:31:45 +0800

    fix: 修复store动态引入问题

--- types/auto-imports.d.ts
+++ types/auto-imports.d.ts
@@ -3,7 +3,6 @@ export {}
 declare global {
   const EffectScope: typeof import('vue')['EffectScope']
   const ElLoading: typeof import('element-plus/es')['ElLoading']
-  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
   const acceptHMRUpdate: typeof import('pinia')['acceptHMRUpdate']
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed']
   const autoResetRef: typeof import('@vueuse/core')['autoResetRef']
