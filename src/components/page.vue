73773d4ee0b97eaf814b6c1b918da23f3e8bed80
yuntian001 <yuntian001z@qq.com>
2024-03-21 18:26:31 +0800

    feat: 加上标准布局页面组件

---
+++ src/components/page.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="page page-100 page-padding">
+      <el-card v-if="$slots.searchForm" class="seach-header">
+        <slot name="searchForm"></slot>
+      </el-card>
+      <el-card class="content-body" body-style="height:100%"	>
+        <slot></slot>
+      </el-card>
+      <slot name="extend"></slot>
+    </div>
+  </template>
+  <style lang="scss" scoped>
+  .page{
+    .seach-header{
+        flex-shrink: 0;
+    }
+    .content-body{
+        flex:1;
+        :deep(.me-vxe-table){
+            height: 100%;
+        }
+    }
+    .content-body:nth-child(n+2){
+        margin-top: 10px;
+    }
+  }
+
+  </style>
